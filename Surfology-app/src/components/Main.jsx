6136901a5d15aa8f3f5b96abdde890a7ead72190
Francesco Torchia <francescotorchia10@gmail.com>
2025-03-24 22:38:37 +0100

    created ShopContext, adding products to assets folder

--- Surfology-app/src/components/Main.jsx
+++ Surfology-app/src/components/Main.jsx
@@ -11,7 +11,7 @@ const Main = () => {
             <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
             <p className="font-medium text-sm md:text-base">OUR BESTSELLERS</p>
           </div>
-          <h1 className="text-3xl sm:py-3 lg:text-5xl leading-relaxed">LATEST ARRIVALS</h1>
+          <h1 className="prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed">LATEST ARRIVALS</h1>
           <div className="flex items-center gap-2 mt-3">
             <p className="font-semibold text-sm md:text-base">SHOP NOW</p>
             <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
