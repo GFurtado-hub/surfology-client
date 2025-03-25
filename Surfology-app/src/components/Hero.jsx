63f758f9d2d68ec75f79c2df54ff682b8915ebf9
Francesco Torchia <francescotorchia10@gmail.com>
2025-03-25 18:26:40 +0100

    shop context

--- Surfology-app/src/components/Hero.jsx
+++ Surfology-app/src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
           </div>
           <h1 className="prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed">SURFBOARDS</h1>
           <div className="flex items-center gap-2 mt-3">
-            <p className="font-semibold text-sm md:text-base">ARE HERE</p>
+            <p className="font-semibold text-sm md:text-base">SHOP NOW</p>
             <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
           </div>
         </div>
--- Surfology-app/src/components/Navbar.jsx
+++ Surfology-app/src/components/Navbar.jsx
@@ -29,21 +29,22 @@ const Navbar = () => {
             </ul>
 
             <div className='flex items-center gap-6'>
-                <img src={assets.SearchIcon} className='w-19 cursor-pointer' alt='search-icon'></img>
+    <img src={assets.SearchIcon} className='w-19 cursor-pointer' alt='search-icon'></img>
 
-                <div className='group relative'>
-                    <img src={assets.ProfileIcon} className='w-10 cursor-pointer' alt='profile-icon'></img>
-                    <div className='group-hover:block hidden absolute dropdown-menu right-0  pt-4'>
-                        <div className='flex flex-col gap-2  w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
-                            <p classNaame='cursor-pointer hover:text-black'>My Profile</p>
-                            <p classNaame='cursor-pointer hover:text-black'>Orders</p>
-                            <p classNaame='cursor-pointer hover:text-black'>Log Out</p>
-                        </div>
-                    </div>
-                </div>
+    <div className='group relative'>
+        <img src={assets.ProfileIcon} className='w-10 cursor-pointer' alt='profile-icon'></img>
+        <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+            <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                <p className='cursor-pointer hover:text-black'>My Profile</p>
+                <p className='cursor-pointer hover:text-black'>Orders</p>
+                <p className='cursor-pointer hover:text-black'>Log Out</p>
+            </div>
+        </div>
+    </div>    
+
                 <Link to='/cart' className='relative'>
                     <img src={assets.CartIcon} className='w-12 min w-12' alt='cart-icon'></img>
-                    <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+                    <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>0</p>
                 </Link>
                 
             </div>
